e34ded5b703ccf263fd5c51e17baedc157ecb880
Philip <44257880+philiphand@users.noreply.github.com>
2021-05-21 15:23:21 +0200

    Changed api url

--- src/config/api.ts
+++ src/config/api.ts
@@ -4,7 +4,8 @@ let apiUrl: string = "";
 if (process.env.VIPPS_AGREEMENT_PAGE_API_URL) {
   apiUrl = process.env.VIPPS_AGREEMENT_PAGE_API_URL;
 } else {
-  apiUrl = "https://dev.data.gieffektivt.no";
+  //apiUrl = "https://dev.data.gieffektivt.no";
+  apiUrl = "http://localhost:3000"
 }
 
 export const API_URL = apiUrl;
